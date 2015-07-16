11cc6d3eea14e61288725c2c1f3590073345e960
_nec <nec@shell8.net>
2015-07-16 16:26:46 +0200

    init

--- jms-storage.js
+++ jms-storage.js
@@ -4,12 +4,48 @@
 
 function storage (cmd, source, branch, data, next) {
 
+	console.log(	storage.settings.engine);
 
 	console.log('storage method', cmd, source, branch, data);
+
+
+
+
+	if (!storage.engine) {
+		// init engine
+
+		try {
+			storage.engine = require('./lib/' + storage.settings.engine)(this)
+		} catch (e) {
+			throw Error('No storage engine for "' + storage.settings.engine +'"')
+		}
+	}
+
+
+
 
 	next(null, +new Date())
 
 }
+
+storage.settings = {
+}
+
+
+
+storage.use = function (engine) {
+	storage.settings.engine = engine;
+
+	return storage;
+}
+
+
+
+
+
+
+
+
 
 module.exports = storage;
 
