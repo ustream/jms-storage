6a83ecf0fcb258f7d801c3de7f38ae9deab6b683
_nec <nec@shell8.net>
2015-07-16 15:58:40 +0200

    init

--- jms-storage.js
+++ jms-storage.js
@@ -7,21 +7,30 @@ function storage (cmd, source, branch, data, next) {
 
 	console.log('storage method', cmd, source, branch, data);
 
+	next(null, +new Date())
+
 }
-
-storage.hapiMethod = function () {
-	return [
-		'storage',
-		storage,
-		{
-		 	cache: {
-		 		segment: 'storage',
-		 		expiresIn: 2000
-		 	}
-		}
-	];
-}
-
-
 
 module.exports = storage;
+
+
+
+/*
+
+
+ request.server.methods.storage(
+ 'getmap',
+ moduleRequest.source,
+ moduleRequest.stage,
+ modulelist,
+ onMapResult.bind(null, request, reply, moduleRequest)
+ )
+
+ storage.hmget(
+ ['map', moduleRequest.source, moduleRequest.stage].join(':'),
+ modulelist,
+ onMapResult
+ .bind(null, request, reply, moduleRequest)
+ );
+
+ */
