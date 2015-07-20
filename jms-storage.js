6b654744cc775b6c54326dff953d813155ab6f5e
_nec <nec@shell8.net>
2015-07-20 14:02:55 +0200

    make module more reusable

--- jms-storage.js
+++ jms-storage.js
@@ -1,28 +1,49 @@
 /**
  *
- * @param cmd
- * @param source
- * @param branch
- * @param data
- * @param next
  *
  * @returns {storage}
  */
-function storage (cmd, source, branch, data, next) {
+function storage (storageConfig, logUtil) {
+
 
 	if (!storage.engine) {
+
+		var config = {};
+
+		if (this.settings && this.settings.app && this.settings.app.storage) {
+			config = this.settings.app.storage;
+		} else if (storageConfig) {
+			config = storageConfig
+		}
+
+		var log = function () {};
+
+		if (this.log) {
+			log = this.log;
+		} else if (logUtil) {
+			log = logUtil
+		}
+
 		try {
-			storage.engine = require('./lib/' + storage.settings.engine)(this)
+			storage.engine = require('./lib/' + storage.settings.engine)(config, log)
 		} catch (e) {
 			throw Error('No storage engine for "' + storage.settings.engine +'"')
 		}
 	}
 
+	var cmd = arguments[0],
+		source = arguments[1],
+		branch = arguments[2],
+		next = arguments[arguments.length - 1],
+		data = Array.prototype.slice.call(arguments, 3);
+
+	data.pop();
+
 	if (!storage.engine[cmd]) {
 		throw Error('Method "' + cmd +'" not implemented in storage engine "' + storage.settings.engine +'"')
 	}
 
-	storage.engine[cmd](source, branch, data, next);
+	storage.engine[cmd].apply(storage.engine, [source, branch].concat(data, [next]));
 
 	return storage;
 }
